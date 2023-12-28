95baa49e356cf67ae292db24a363b9300f30a235
papillon107 <teodormiron75@gmail.com>
2023-12-28 20:36:07 +0200

    bug: dupa ce dau o data pe resume, mi se duce cursoul pe new game de asta merge sa dau de 2 ori click

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 #include <curses.h>
 #include <time.h>
+#include <stdlib.h>
 #define star '*'
 
 char* timestr(struct tm *t, char* time){
@@ -110,20 +111,55 @@ int selectOption(int height, int width, char* title, char *ng, char *res, char *
     return 0;
 }
 
-void drawTable(WINDOW *window){
+void drawTable(WINDOW *window, int table[4][4]){
     int height, width;
     getmaxyx(window, height, width);
-    int y = height / 2 - 6; // Start drawing the table in the middle of the window
-    int x = (width / 2) - width / 4;
-    wmove(window, y, x);
-    // Draw the top border of the table
-    whline(window, ACS_HLINE, 2 * (width / 4));
+    int squareSize = width / 6; // Size of the square based on the width of the window
+    int y = (height / 2) - (squareSize / 2); // Start drawing the table in the middle of the window
+    int x = (width / 2) - (squareSize * 2); // Center the table horizontally
+
+    // Draw the outer square
+    for (int i = 0; i < squareSize + 1; i++) {
+        mvwaddch(window, y + i, x, ACS_VLINE);
+        mvwaddch(window, y + i, x + squareSize * 4, ACS_VLINE);
+    }
+    mvwhline(window, y, x, ACS_HLINE, squareSize * 4 + 1);
+    mvwhline(window, y + squareSize, x, ACS_HLINE, squareSize * 4 + 1);
+
+    // Draw the inner lines
+    for (int i = 1; i < 4; i++) {
+        mvwhline(window, y + i * squareSize / 4, x, ACS_HLINE, squareSize * 4);
+        mvwvline(window, y, x + i * squareSize, ACS_VLINE, squareSize);
+    }
+
+    // Draw the values of the matrix
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (table[i][j] != 0) {
+                mvwprintw(window, y + i * squareSize / 4 + 1, x + j * squareSize + 1, "%d", table[i][j]);
+            }
+        }
+    }
 
     wrefresh(window); // Refresh the window to show the table
 }
 
-WINDOW* NewGame(int height, int width, int *score){
-    WINDOW *gameWindow = newwin(height, width, 0, 0);
+void continueGame(WINDOW* game, int *score){
+    touchwin(game);
+    wrefresh(game);
+    int c;
+    while ((c = getch()) != 'q'){
+        //do stuff
+    }
+    
+    if (tolower(c) == 'q'){
+        touchwin(stdscr);
+        wrefresh(stdscr);
+    }
+}
+
+WINDOW* NewGame(int height, int width, int *score, int table[4][4]){
+    WINDOW *gameWindow = newwin(24, 80, 0, 0);
     char t[20];
     int c;
     time_t now = time(NULL);
@@ -132,31 +168,45 @@ WINDOW* NewGame(int height, int width, int *score){
     box(gameWindow, 0, 0); // Draw a box around the window
     mvwprintw(gameWindow, 1, width / 2 - 2, "2048"); // Print a message in the new window
     mvwprintw(gameWindow, 2, (width / 2) - strlen("2048") / 2 - 2, "Score: %d", score);
-    
-    drawTable(gameWindow);
-    
+    drawTable(gameWindow, table);
     wrefresh(gameWindow); // Refresh the window
+
     while ((c = getch()) != 'q'){
         //do stuff
     }
-    if (c == 'q'){
+    
+    if (tolower(c) == 'q'){
         touchwin(stdscr);
         wrefresh(stdscr);
     }
     return gameWindow;
 }
 
-int main(){
-	int width;
+void initTable(int table[4][4]){
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            table[i][j] = 0;
+        }
+    }
+    int i;
+    for (i = 0; i < 2; i++) {
+        int x = rand() % 4;
+        int y = rand() % 4;
+        table[x][y] = (rand() % 2 + 1) * 2;
+    }
+}
+
+int main() {
+    int width;
     int height;
     char* title = "2048 made by Adrian";
     char* ng = "New Game";
     char* res = "Resume";
     char* q = "Quit";
-    int *score = 0;
+    int* score = NULL;
+    int table[4][4] = {0};
 
-    WINDOW *mainWindow = initscr(); //initializez fereastra de joc
-    raw();
+    WINDOW* mainWindow = initscr(); //initializez fereastra de joc
     getmaxyx(mainWindow, height, width);
     clear(); //sterg ecranul
     noecho();
@@ -165,31 +215,34 @@ int main(){
     keypad(stdscr, TRUE);
 
     setupScreen(height, width, title, ng, res, q);
-    WINDOW *gameWindow = NULL;
-    while(1) {
-        switch(selectOption(height, width, title, ng, res, q)){
+    WINDOW* gameWindow = NULL;
+    while (1) {
+        switch (selectOption(height, width, title, ng, res, q)) {
         case 1:
-            gameWindow = NewGame(height, width, score);
-            mvaddstr(2, 3, " hey there bud'! let's play!  ");
-            refresh();
-            break;
-        case 2: 
             if (gameWindow != NULL){
-                if (gameWindow != NULL){
-                touchwin(gameWindow);
-                wrefresh(gameWindow);
-                }
+                delwin(gameWindow);
             }
-            else {
-                mvaddstr(2, 3, " sorry bud', you can't resume");
-                refresh();
-            }
+            initTable(table);
+            gameWindow = NewGame(height, width, score, table);
+            mvaddstr(2, 3, " hey there bud! let's play!  ");
             break;
+        case 2:
+            if (gameWindow != NULL) {
+                continueGame(gameWindow, score);
+                break;
+            } else {
+                mvaddstr(2, 3, " sorry bud, you can't resume ");
+                refresh();
+                break;
+            }
         case 3:
+            if (gameWindow != NULL) {
+                delwin(gameWindow); // Delete the game window if it exists
+            }
             endwin();
             return 0;
         }
     }
     endwin();
-	return 0;
+    return 0;
 }
